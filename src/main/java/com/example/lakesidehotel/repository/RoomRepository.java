e9fd8459d85db05602562279220e1de630672e23
DhruvTankalwala <tankalwaladhruv@gmail.com>
2025-02-21 02:54:54 +0530

    Add New Room Functionality

--- src/main/java/com/example/lakesidehotel/repository/RoomRepository.java
+++ src/main/java/com/example/lakesidehotel/repository/RoomRepository.java
@@ -1,6 +1,9 @@
 package com.example.lakesidehotel.repository;
 
-public interface RoomRepository {
+import com.example.lakesidehotel.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room,Long> {
 
 
 }
